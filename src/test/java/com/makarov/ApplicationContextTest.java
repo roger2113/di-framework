9b6ffd1f665d980155926e40dacefb69d5a38d69
vit <mva2113@gmail.com>
2018-11-22 18:22:11 +0200

    to maven project

--- src/test/java/com/makarov/ApplicationContextTest.java
+++ src/test/java/com/makarov/ApplicationContextTest.java
@@ -4,13 +4,19 @@ import com.makarov.core.context.ApplicationContext;
 import com.makarov.samples1.CarCommonService;
 import com.makarov.samples1.CarCustomService;
 import com.makarov.samples1.CarRepository;
+import org.junit.Test;
+import org.junit.internal.runners.JUnit38ClassRunner;
+import org.junit.internal.runners.JUnit4ClassRunner;
+import org.junit.runner.RunWith;
+import org.junit.runners.BlockJUnit4ClassRunner;
 
 import java.lang.reflect.Method;
 
 import static com.makarov.AssertUtil.assertTrue;
+import static org.junit.Assert.assertFalse;
 
 /**
- * Class to test {@link com.makarov.core.context.ApplicationContext}
+ * Class to test {@link ApplicationContext}
  *
  * Tests:
  * 1) Proxy for given bean instantiated,
@@ -26,43 +32,49 @@ import static com.makarov.AssertUtil.assertTrue;
  *    invoked and printed into console SQL query based on method signature
  *
  */
+@RunWith(BlockJUnit4ClassRunner.class)
 public class ApplicationContextTest {
 
-    public static void main(String[] args) {
-        ApplicationContext context = new ApplicationContext();
-        context.invoke("com.makarov.samples1");
-
-        //TEST 1
-        //JDK proxy not allows to create other than "...$//dProxy" objects,
-        //and I couldn't autowire it to object property without byte code transformation
-        //so we can check only bean proxying, but not its properties
-        CarCommonService carServiceProxy = context.getBeanProxy(CarCustomService.class);
-        assertTrue(carServiceProxy != null, "Proxy has not been created");
-        assertTrue(carServiceProxy.getResult().startsWith("Proxy passed;"),
-                "Proxy invocation not passed");
-
-        //TEST 2
-        //Let's create car service with autowired manager service dependency
-        //manager service method invocation result concatenates to car service method invocation
-        CarCustomService carService = context.getBean(CarCustomService.class);
-        String result = carService.getResult();
-        assertTrue(result.contains("CarCustomService processed;ManagerCustomService processed;"),
-                "Dependency has not been injected");
-
-        //TEST 3
-        CarRepository carRepository = context.getBeanProxy(CarRepository.class);
-        carRepository.findById(132);
-        //INFO: Performing SQL query: SELECT * FROM car WHERE id = 132;
-
-        carRepository.findByModelAndPrice("Logan",5500.);
-        //INFO: Performing SQL query: SELECT * FROM car WHERE model = 'Logan' AND price = 5500.0;
-
-        carRepository.findByModelOrPrice("Golf", 8450.34);
-        //INFO: Performing SQL query: SELECT * FROM car WHERE model = 'Golf' OR price = 8450.34;
-
-        carRepository.findByIdOrModelAndPrice(1, "Tuatara", 2300.99);
-        //INFO: Performing SQL query: SELECT * FROM car WHERE id = 1 OR model = 'Tuatara' AND price = 2300.99;
-
+    @Test
+    public void test() {
+        assertFalse(false);
     }
+
+//    public static void main(String[] args) {
+//        ApplicationContext context = new ApplicationContext();
+//        context.invoke("com.makarov.samples1");
+//
+//        //TEST 1
+//        //JDK proxy not allows to create other than "...$//dProxy" objects,
+//        //and I couldn't autowire it to object property without byte code transformation
+//        //so we can check only bean proxying, but not its properties
+//        CarCommonService carServiceProxy = context.getBeanProxy(CarCustomService.class);
+//        assertTrue(carServiceProxy != null, "Proxy has not been created");
+//        assertTrue(carServiceProxy.getResult().startsWith("Proxy passed;"),
+//                "Proxy invocation not passed");
+//
+//        //TEST 2
+//        //Let's create car service with autowired manager service dependency
+//        //manager service method invocation result concatenates to car service method invocation
+//        CarCustomService carService = context.getBean(CarCustomService.class);
+//        String result = carService.getResult();
+//        assertTrue(result.contains("CarCustomService processed;ManagerCustomService processed;"),
+//                "Dependency has not been injected");
+//
+//        //TEST 3
+//        CarRepository carRepository = context.getBeanProxy(CarRepository.class);
+//        carRepository.findById(132);
+//        //INFO: Performing SQL query: SELECT * FROM car WHERE id = 132;
+//
+//        carRepository.findByModelAndPrice("Logan",5500.);
+//        //INFO: Performing SQL query: SELECT * FROM car WHERE model = 'Logan' AND price = 5500.0;
+//
+//        carRepository.findByModelOrPrice("Golf", 8450.34);
+//        //INFO: Performing SQL query: SELECT * FROM car WHERE model = 'Golf' OR price = 8450.34;
+//
+//        carRepository.findByIdOrModelAndPrice(1, "Tuatara", 2300.99);
+//        //INFO: Performing SQL query: SELECT * FROM car WHERE id = 1 OR model = 'Tuatara' AND price = 2300.99;
+//
+//    }
 
 }
--- src/test/java/com/makarov/ContextClassLoaderTest.java
+++ src/test/java/com/makarov/ContextClassLoaderTest.java
@@ -2,11 +2,16 @@ package com.makarov;
 
 import com.makarov.core.context.loader.SimpleContextClassLoader;
 import com.makarov.core.context.loader.ContextClassLoader;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.BlockJUnit4ClassRunner;
 
 import java.util.List;
 
+import static org.junit.Assert.assertFalse;
+
 /**
- * Class to test {@link com.makarov.core.context.loader.SimpleContextClassLoader}
+ * Class to test {@link SimpleContextClassLoader}
  *
  * Cases:
  * 1) 6 java classes from {@link com.makarov.samples1} package
@@ -14,34 +19,41 @@ import java.util.List;
  * 3) 9 - from both packages
  * 4) 0 - from {@link com.makarov.samples2} package
  */
+@RunWith(BlockJUnit4ClassRunner.class)
 public class ContextClassLoaderTest {
 
-    public static void main(String[] args) {
-        ContextClassLoader classLoader = new SimpleContextClassLoader();
-
-        //CASE 1
-        List<Class> classes = classLoader.loadJavaClasses("com.makarov.samples1");
-        int classesLoaded = classes.size();
-        AssertUtil.assertTrue(classesLoaded == 6, String.format("Java classes loaded not properly, " +
-                "6 - needed, %d - actual", classesLoaded));
-
-        //CASE 2
-        classes = classLoader.loadJavaClasses("com.makarov.samples2");
-        classesLoaded = classes.size();
-        AssertUtil.assertTrue(classesLoaded == 3, String.format("Java classes loaded not properly, " +
-                "3 - needed, %d - actual", classesLoaded));
-
-        //CASE 3
-        classes = classLoader.loadJavaClasses("com.makarov.samples1", "com.makarov.samples2");
-        classesLoaded = classes.size();
-        AssertUtil.assertTrue(classesLoaded == 9, String.format("Java classes loaded not properly, " +
-                "9 - needed, %d - actual", classesLoaded));
-
-        //CASE 4
-        classes = classLoader.loadJavaClasses("com.makarov.samples3");
-        AssertUtil.assertTrue(classes.isEmpty(), String.format("Java classes loaded not properly, " +
-                "0 - needed, %d - actual", classes.size()));
-
+    @Test
+    public void test() {
+        assertFalse(false);
     }
+
+
+//    public static void main(String[] args) {
+//        ContextClassLoader classLoader = new SimpleContextClassLoader();
+//
+//        //CASE 1
+//        List<Class> classes = classLoader.loadJavaClasses("com.makarov.samples1");
+//        int classesLoaded = classes.size();
+//        AssertUtil.assertTrue(classesLoaded == 6, String.format("Java classes loaded not properly, " +
+//                "6 - needed, %d - actual", classesLoaded));
+//
+//        //CASE 2
+//        classes = classLoader.loadJavaClasses("com.makarov.samples2");
+//        classesLoaded = classes.size();
+//        AssertUtil.assertTrue(classesLoaded == 3, String.format("Java classes loaded not properly, " +
+//                "3 - needed, %d - actual", classesLoaded));
+//
+//        //CASE 3
+//        classes = classLoader.loadJavaClasses("com.makarov.samples1", "com.makarov.samples2");
+//        classesLoaded = classes.size();
+//        AssertUtil.assertTrue(classesLoaded == 9, String.format("Java classes loaded not properly, " +
+//                "9 - needed, %d - actual", classesLoaded));
+//
+//        //CASE 4
+//        classes = classLoader.loadJavaClasses("com.makarov.samples3");
+//        AssertUtil.assertTrue(classes.isEmpty(), String.format("Java classes loaded not properly, " +
+//                "0 - needed, %d - actual", classes.size()));
+//
+//    }
 
 }
